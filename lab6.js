ff029a299dc0f2eb5779e13e5d704a31ccf101de
Kirsten Kellman <kirstenkellman@gmail.com>
2018-11-14 00:27:59 +0000

    finally got exercise 3 to work

--- lab6.js
+++ lab6.js
@@ -2,20 +2,35 @@
 window.onload = function () {
     // body...
 
- var search = document.getElementById("button");
- var str = document.getElementsByName("Term");
+ var search = document.getElementById("button1");
+ var str = document.getElementById("Term");
+ var all = document.getElementById("button2");
  
-      search.onclick = function Search(str) {
+   document.getElementById("Term").addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("button1").click();
+    }
+});
+ 
+      search.onclick = function Search() {
+          
+           /*if (str.length==0) { 
+               document.getElementById("result").innerHTML="";
+               document.getElementById("result").style.border="0px";
+               return;
+            }*/
           // body...
+           var url = "request.php?q="+str.value;
           var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-            // document.getElementById("demo").innerHTML =
+             document.getElementById("result").innerHTML = this.responseText;
             
-             alert( this.responseText);
+    
             }
           };
-            xhttp.open("GET", "request.php?q="+str, true);
-            xhttp.send();
+            xhttp.open("GET", url, true);
+            xhttp.send(null);
          }
 }
