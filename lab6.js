2e61867259ee329717514f4274719964e77a1638
Kirsten Kellman <kirstenkellman@gmail.com>
2018-11-14 02:15:33 +0000

    exercise four before order list

--- lab6.js
+++ lab6.js
@@ -33,4 +33,19 @@ window.onload = function () {
             xhttp.open("GET", url, true);
             xhttp.send(null);
          }
+         
+         all.onclick = function Everything(){
+             var url1 = "request.php?q=&all=true";
+              var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+             document.getElementById("result").innerHTML = this.responseText;
+            
+    
+            }
+          };
+            xhttp.open("GET", url1, true);
+            xhttp.send(null);
+         }
+         
 }
